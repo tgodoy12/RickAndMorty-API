f008ef5aa9f22735da1e12189f1e62f7835a9c0c
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-09 21:10:12 +0000

    all rick and morty characters captured

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [allCharacters, setAllCharacters] = useState({});
+	const [singleCharacter, setSingleCharacter] = useState({});
+
+	const getAllCharacters = () => {
+		fetch("https://rickandmortyapi.com/api/character")
+			.then((response) => {
+				if (!response.ok) throw Error(response.statusText);
+				return response.json();})
+			.then((data) => setAllCharacters(data.results))
+			.catch((error) => console.log(error));
+	}
+
+	console.log(allCharacters[0]);
+
+	useEffect(() => {
+		getAllCharacters();
+	},[]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			{/* <ul>
+				<li>{singleCharacter.name}</li>
+			</ul> */}
 		</div>
 	);
 };
